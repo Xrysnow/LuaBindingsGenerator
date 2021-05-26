d9173643ea4ad80029a32a440d37765e67e30b44
Xrysnow <xrysnow@outlook.com>
2021-05-26 11:16:39 +0800

    Create fgui.h

---
+++ tolua/headers/fgui.h
@@ -0,0 +1,8 @@
+#include "FairyGUI.h"
+#include "GLoader3D.h"
+#include "tween/EaseManager.h"
+#include "tween/GPath.h"
+#include "display/FUISprite.h"
+#include "utils/html/HtmlElement.h"
+#include "utils/html/HtmlObject.h"
+#include "utils/html/HtmlParser.h"
