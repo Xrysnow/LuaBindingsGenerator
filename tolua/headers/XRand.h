b7bd1756aea6583ea3b478b815ec88c1a657c7d2
Xrysnow <xrysnow@outlook.com>
2021-05-26 11:16:25 +0800

    Update XRand.h

--- tolua/headers/XRand.h
+++ tolua/headers/XRand.h
@@ -3,11 +3,23 @@
 
 namespace lstg
 {
-	class Random : public cocos2d::Ref
+	class Random
 	{
 	public:
-		static Random* create();
-		static Random* create(uint32_t seed);
+		enum class GeneratorType
+		{
+			well512,
+			minstd_rand0, minstd_rand,
+			mt19937, mt19937_64,
+			ranlux24_base, ranlux48_base,
+			ranlux24, ranlux48,
+			knuth_b
+		};
+
+		// static Random* create();
+		// static Random* create(
+		// 	uint32_t seed, GeneratorType type = GeneratorType::well512);
+		static uint32_t getDeviceSeed();
 
 		void setSeed(uint32_t seed);
 		uint32_t getSeed();
@@ -83,9 +95,44 @@ namespace lstg
 			 * alpha is the scale parameter and beta is the shape parameter.*/
 			double weibull(double alpha, double beta);
 
-	public:
-		virtual ~Random();
-		Random(const Random &) = delete;
-		Random &operator =(const Random &) = delete;
+
+		int64_t uniform_int(int64_t a, int64_t b);
+		/** produces bool values on a Bernoulli distribution. */
+		bool bernoulli(double p);
+		/** produces integer values on a binomial distribution. */
+		int32_t binomial(int32_t t, double p);
+		/** produces integer values on a negative binomial distribution. */
+		int32_t negative_binomial(int32_t k, double p);
+		/** produces integer values on a geometric distribution. */
+		int32_t geometric(double p);
+
+		/** produces integer values on a poisson distribution. */
+		int32_t poisson(double mean);
+		/** produces real values on an extreme value distribution. */
+		double extreme_value(double a, double b);
+
+		/** produces real values on a chi-squared distribution. */
+		double chi_squared(double n);
+		/** produces real values on a Cauchy distribution. */
+		double cauchy(double a, double b);
+		/** produces real values on a Fisher's F-distribution. */
+		double fisher_f(double m, double n);
+		/** produces real values on a Student's t-distribution. */
+		double student_t(double n);
+
+		/** produces random integers on a discrete distribution. */
+		int32_t discrete(const std::vector<double>& weights);
+		/** produces real values distributed on constant subintervals. */
+		double piecewise_constant(
+			const std::vector<double>& sequence,
+			const std::vector<double>& weights);
+		/** produces real values distributed on defined subintervals. */
+		double piecewise_linear(
+			const std::vector<double>& sequence,
+			const std::vector<double>& weights);
+
+
+		Random(uint32_t seed = 0, GeneratorType type = GeneratorType::well512);
+
 	};
 }
